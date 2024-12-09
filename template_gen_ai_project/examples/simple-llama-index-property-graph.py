1158a31cc983b997eb00cf49500cee60ba812394
Isaiah Swain <Isaiah.Swain@cognizant.com>
2024-12-09 22:20:33 +0000

    Created Property Graph example

---
+++ template_gen_ai_project/examples/simple-llama-index-property-graph.py
@@ -0,0 +1,112 @@
+# template_gen_ai_project/examples/simple-llama-index-neo4j-graph.py
+# https://docs.llamaindex.ai/en/stable/examples/index_structs/knowledge_graph/Neo4jKGIndexDemo/
+from llama_index.core import StorageContext
+from llama_index.core.indices import PropertyGraphIndex
+from llama_index.graph_stores.neo4j import Neo4jPropertyGraphStore
+from template_gen_ai_project.settings import settings
+from template_gen_ai_project.helpers.logger_helper import get_logger
+from llama_index.core import Settings as LlamaIndexSettings
+from llama_index.llms.azure_openai import AzureOpenAI
+from template_gen_ai_project.helpers.embeddings import (
+    setup_embeddings,
+    Library,
+    ModelProvider,
+)
+from llama_index.readers.wikipedia import WikipediaReader
+
+# Get the configured logger
+logger = get_logger()
+
+llm = AzureOpenAI(
+    model=settings.AZURE_OPENAI_MODEL,
+    temperature=0,
+    deployment_name=settings.AZURE_OPENAI_DEPLOYMENT_NAME,
+    api_key=settings.AZURE_OPENAI_API_KEY,
+    azure_endpoint=settings.AZURE_OPENAI_ENDPOINT,
+    api_version=settings.AZURE_OPENAI_API_VERSION,
+)
+
+LlamaIndexSettings.llm = llm
+LlamaIndexSettings.embed_model = setup_embeddings(
+    library=Library.LlamaIndex, model_provider=ModelProvider.AzureOpenAI
+)
+LlamaIndexSettings.chunk_size = 512
+
+# global
+username = "neo4j"
+password = "pleaseletmein"
+url = "bolt://localhost:7687"
+database = "neo4j"
+embed_dim = 1536
+test_data_dir = "./test_data/financial_docs"
+index_cache_dir = "./knowledge_graph_index"
+
+# Prepare documents
+loader = WikipediaReader()
+
+logger.info("Loading Wikipedia data")
+documents = loader.load_data(
+    pages=["Guardians of the Galaxy Vol. 3"],
+    auto_suggest=False,
+)
+
+logger.info(f"Connecting to Neo4j at {url}")
+graph_store = Neo4jPropertyGraphStore(
+    username=username,
+    password=password,
+    url=url,
+    database=database,
+)
+
+logger.info("Creating storage context")
+storage_context = StorageContext.from_defaults(graph_store=graph_store)
+
+
+# NOTE: can take a while!
+logger.info("Creating index")
+index = PropertyGraphIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+    # max_triplets_per_chunk=2, # Makes ingestion faster, but not as good
+    show_progress=True,
+    property_graph_store=graph_store,
+)
+storage_context.persist(persist_dir="./.cache/knowledge_graph_index")
+
+# logger.info(f"Loading index from {index_cache_dir}")
+# index = load_index_from_storage(
+#     StorageContext.from_defaults(persist_dir=index_cache_dir)
+# )
+
+
+###########################
+
+
+query_engine = index.as_query_engine(
+    # include_text=False,
+    # response_mode="tree_summarize",
+    # embedding_mode="hybrid",
+    # similarity_top_k=5,
+    verbose=True,
+)
+
+logger.info("Querying with LLM")
+
+
+def query_graph_store(query):
+    print("\n")
+    logger.info(f"User: {query}")
+    print(f"Agent: {query_engine.query(query)}")
+    print("\n")
+
+
+# Example queries
+# query_graph_store("How was the quarter?")
+# query_graph_store("What is the document about?")
+
+# Interactive chat interface
+while True:
+    user_query = input("User: ")
+    if user_query.lower() == "quit":
+        break
+    query_graph_store(user_query)
--- template_gen_ai_project/examples/simple_llama_index_azure_query.py
+++ template_gen_ai_project/examples/simple_llama_index_azure_query.py
@@ -1,7 +1,25 @@
+import os
+
 from llama_index.llms.azure_openai import AzureOpenAI
 
+from llama_index.core import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    StorageContext,
+    load_index_from_storage,
+)
 from llama_index.core import Settings as LlamaIndexSettings
+from llama_index.core import get_response_synthesizer
+from llama_index.core.node_parser.text.token import TokenTextSplitter
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
 from template_gen_ai_project.settings import settings
+from template_gen_ai_project.helpers.logger_helper import get_logger
+
+logger = get_logger()
+PERSIST_DIR = "./cache"
 
 LlamaIndexSettings.llm = AzureOpenAI(
     engine=settings.AZURE_OPENAI_DEPLOYMENT_NAME,
@@ -10,3 +28,81 @@ LlamaIndexSettings.llm = AzureOpenAI(
     azure_endpoint=settings.AZURE_OPENAI_ENDPOINT,
     api_version=settings.AZURE_OPENAI_API_VERSION,
 )
+
+LlamaIndexSettings.embed_model = HuggingFaceEmbedding(
+    model_name="BAAI/bge-small-en-v1.5"
+)
+
+if not os.path.exists(PERSIST_DIR):
+    logger.info("Cache doesn't exist. Creating cache")
+
+    logger.info("Ingesting Documents")
+    docs = SimpleDirectoryReader(input_files=["./data/10k/lyft_2021.pdf"]).load_data()
+    parser = TokenTextSplitter()
+    pipeline = IngestionPipeline(
+        transformations=[
+            parser,
+            HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"),
+        ]
+    )
+    nodes = pipeline.run(documents=docs, show_progress=True)
+
+    logger.info("Building index")
+    lyft_index = VectorStoreIndex(nodes)
+
+    lyft_index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    logger.info("Cache detected. Loading existing index")
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    lyft_index = load_index_from_storage(storage_context)
+
+logger.info("Retrieving Nodes")
+retriever = VectorIndexRetriever(index=lyft_index, similarity_top_k=3)
+
+query = "What information does this document contain?"
+
+nodes = retriever.retrieve(query)
+
+print(type(nodes[0]))
+
+for node in nodes:
+    print(f"Node ID: {node.node_id}, Text: {node.text}")
+
+response_synthesizer = get_response_synthesizer(response_mode="compact")
+
+response = response_synthesizer.synthesize(query, nodes)
+
+print(response)
+
+# query_engine_tools = [
+#    QueryEngineTool(
+#        query_engine=lyft_engine,
+#        metadata=ToolMetadata(
+#            name="lyft_10k",
+#            description=(
+#                "Provides information about Lyft financials for year 2021"
+#                "Provides information about Lyft financials"
+#            )
+#        )
+#    ),
+#    QueryEngineTool(
+#        query_engine=uber_engine,
+#        metadata=ToolMetadata(
+#            name="uber_10k",
+#            description=(
+#                "Provides information about Uber financials for year 2021"
+#            )
+#        )
+#    )
+# ]
+#
+# s_engine = SubQuestionQueryEngine.from_defaults(
+#    query_engine_tools=query_engine_tools,
+#    verbose=True
+# )
+#
+# logger.info("Running queries")
+# response = s_engine.query(
+#    "What information does this document contain?"
+# )
+# print(response)
